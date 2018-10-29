b7e899d21c5f8454c5d9d96a4c5af760bc2b3ce4
Jesse Hoogervorst <hoogervorstjesse@gmail.com>
2018-10-29 13:08:06 +0100

    Added socket.io

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,18 @@
-// Get dependencies
-const express = require('express');
-const path = require('path');
-const http = require('http');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-
-// Get API routes
-const api = require('./server/routes/api');
-
 //Connect to database
+const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://' + (process.env.MONGO_HOST || 'localhost') + '/projectBetrayal', { useNewUrlParser: true, useCreateIndex: true });
 
+//Create the express app
+const express = require('express');
 const app = express();
 
+// Store Epress port
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
 // POST data parsers
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -26,9 +24,11 @@ app.use(function (req, res, next) {
 });
 
 // Point static path to dist
+const path = require('path');
 app.use(express.static(path.join(__dirname, 'dist')));
 
-//Setup our api routes
+//Setup API routes
+const api = require('./server/routes/api');
 app.use('/api', api);
 
 // All other routes go to the index file
@@ -36,12 +36,13 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'))
 });
 
-// Store Epress port
-const port = process.env.PORT || 3000;
-app.set('port', port);
-
 //Create HTTP server
+const http = require('http');
 const server = http.createServer(app);
+
+//Create the socket.io handler
+const io = require('socket.io').listen(server)
+io.sockets.on("connect", () => { });
 
 //Listen on provided port, on all interfaces
 server.listen(port, () => console.log('Server and API are running on localhost:' + port));
